7f1b1076d866ca5502541b123f566ca4334c10b0
Krzysiu <kheciek@gmail.com>
2017-09-06 18:57:07 +0200

    Added spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 var a=prompt('Podaj pierwszą liczbę'),
     b=prompt('Podaj drugą liczbę'),
-    value=(a*a)+(2*a*b)-(b*b);
+    value=(a * a) + (2 * a * b) - (b * b);
 
-if (value>0) {
+if (value > 0) {
     console.log('Wynik dodatni');
-} else if (value<0) {
+} else if (value < 0) {
     console.log('Wynik ujemny');
 } else {
     console.log('Wynik równy zeru');
